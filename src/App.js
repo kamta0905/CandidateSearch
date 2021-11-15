bae20597aefb8460ebd48c82f388bcb3405748b1
kamta <kamtap302@gmail.com>
2021-11-16 03:07:38 +0530

    fsdhi

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React, { useState, useEffect } from 'react';
 function App() {
+  const [users, setUsers] = useState([]);
+  const [search, searchTerm] = useState("")
+  const getUsers = async () => {
+    const response = await fetch('https://s3-ap-southeast-1.amazonaws.com/he-public-data/users49b8675.json');
+    setUsers(await response.json())
+  }
+  useEffect(() => {
+    getUsers();
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div>
+      <input className="search" type="text" placeholder="Search by name" onChange={(event) => {
+        searchTerm(event.target.value)
+      }} />
+      <button className="btn">Search</button>
+
+      {
+        users.filter((i) => {
+          if (search === "") {
+            return i;
+          } else if (i.name.toLowerCase().includes(search.toLowerCase())) {
+            return i
+          }
+        }).map((i, key) => {
+          return (
+            <>
+              <div className="all" key={key}>
+                <div className="image" ><img src={i.Image} /></div>
+                <div className="name">{i.name}</div>
+                <div className="id">{i.id}</div>
+              </div>
+
+
+            </>
+          )
+        })
+
+      }
+
+    </div >
+  )
 }
 
 export default App;
